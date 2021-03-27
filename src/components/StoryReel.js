6053aefce7f17268e78ee59711f857bb76899751
sakthivel-happyfox <sakthivel@happyfox.com>
2021-03-27 22:50:48 +0530

    Header, Sidebar and Story reel update

---
+++ src/components/StoryReel.js
@@ -0,0 +1,39 @@
+import React from "react";
+import "../StoryReel.css";
+import Story from "./Story";
+
+function StoryReel() {
+  return (
+    <div className="storyReel">
+      <Story
+        image="https://scontent.fmaa12-1.fna.fbcdn.net/v/t1.0-0/s280x280/165365579_289981045822285_2011849057894989208_o.jpg?_nc_cat=100&ccb=1-3&_nc_sid=f7b297&_nc_ohc=iRVj3tknoRoAX-e-bBA&_nc_ht=scontent.fmaa12-1.fna&tp=7&oh=dc9485df41d512be855bcef0b8b63de2&oe=608341C3"
+        profileSrc="https://scontent.fmaa12-1.fna.fbcdn.net/v/t1.0-1/cp0/c0.0.40.40a/p40x40/164360683_288469305973459_3748363076912628779_o.jpg?_nc_cat=1&ccb=1-3&_nc_sid=c6021c&_nc_ohc=_9Uv7IBtZbMAX_y2VqB&_nc_ht=scontent.fmaa12-1.fna&tp=27&oh=518d0daebae8e7de738964c768ddc8d2&oe=6083829B"
+        title="Myrtle Sarrosa"
+      />
+      <Story
+        image="https://scontent.fmaa12-1.fna.fbcdn.net/v/t15.5256-10/s280x280/121937159_4230064963678533_9209088544460156026_n.jpg?_nc_cat=1&ccb=1-3&_nc_sid=1055be&_nc_ohc=j32BJwAZmsEAX95nb2W&_nc_ht=scontent.fmaa12-1.fna&tp=7&oh=631149f1358c990cd945798ebea091df&oe=6084AB95"
+        profileSrc="https://scontent.fmaa12-1.fna.fbcdn.net/v/t1.0-1/cp0/c0.0.40.40a/p40x40/141792320_260284695458130_6951321349552906650_n.jpg?_nc_cat=1&ccb=1-3&_nc_sid=c6021c&_nc_ohc=5sVDdeiBRjsAX9kbFdz&_nc_ht=scontent.fmaa12-1.fna&tp=27&oh=850c8f3e304c95a27bbb20c636ca2aae&oe=60832B55"
+        title="Refeh Qazi"
+      />
+      <Story
+        image="https://scontent.fmaa12-1.fna.fbcdn.net/v/t15.5256-10/s280x280/164338071_197968105084365_2853877538931299471_n.jpg?_nc_cat=103&ccb=1-3&_nc_sid=ad6a45&_nc_ohc=xnhcep_EiWgAX-QUDn1&_nc_ht=scontent.fmaa12-1.fna&tp=7&oh=5ed8210a01e025daed6dc55a4a1544fe&oe=6086388B"
+        profileSrc="https://scontent.fmaa12-1.fna.fbcdn.net/v/t1.0-1/cp0/p40x40/160269140_287285859427829_8284170307313797885_n.jpg?_nc_cat=1&ccb=1-3&_nc_sid=c6021c&_nc_ohc=FMQamDWt5oIAX-3q2oe&_nc_ht=scontent.fmaa12-1.fna&tp=27&oh=f51bd3f9c3f6c14c106c9d273df97ab0&oe=60856F51"
+        title="Frankie"
+      />
+
+      <Story
+        image="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTBHC2s4NFdzXEsVzvBPGjkrSePQa-8XFuNtQ&usqp=CAU"
+        profileSrc="https://scontent.fmaa12-1.fna.fbcdn.net/v/t1.0-1/cp0/p40x40/78553163_2177128382588608_46505068362465280_o.jpg?_nc_cat=106&ccb=1-3&_nc_sid=7206a8&_nc_ohc=POCTMb-hzOQAX-dasI9&_nc_ht=scontent.fmaa12-1.fna&tp=27&oh=f7ad77937d4f1992e9929e8d0ec79f6d&oe=608533C4"
+        title="Aaron Bernath"
+      />
+
+      <Story
+        image="https://scontent.fmaa12-1.fna.fbcdn.net/v/t1.0-0/s280x280/163608044_1233672503714593_673250086579161192_o.jpg?_nc_cat=104&ccb=1-3&_nc_sid=365331&_nc_ohc=ca61jK1d0pQAX9pBDDw&_nc_ht=scontent.fmaa12-1.fna&tp=7&oh=1b4ece9103012c462c2063a15cd212b1&oe=6084BA4B"
+        profileSrc="https://scontent.fmaa12-1.fna.fbcdn.net/v/t1.0-1/cp0/p40x40/55441211_2413088715578675_451938930154012672_o.jpg?_nc_cat=102&ccb=1-3&_nc_sid=7206a8&_nc_ohc=pEvVNHsX5qcAX_UWHnn&_nc_ht=scontent.fmaa12-1.fna&tp=27&oh=a17bfbc10b1a250347bf9bbc055ae5fb&oe=6083F6C3"
+        title="Naz"
+      />
+    </div>
+  );
+}
+
+export default StoryReel;
